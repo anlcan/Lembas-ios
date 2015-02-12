bff450a11a6e0622f11584bdf1d8a0ed6b0dd632
anlcan <anilcanbaykal@gmail.com>
2015-02-12 19:25:42 +0200

    removed default skipped token & service fields from marshalling

---
+++ Example/HBDLembasTest/HBDLembasTestTests/LembasTestRequest.h
@@ -0,0 +1,13 @@
+//
+//  LembasTestRequest.h
+//  HBDLembasTest
+//
+//  Created by Anil Can Baykal on 05/12/14.
+//  Copyright (c) 2014 Anil Can Baykal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LembasTestRequest : NSObject
+
+@end
---
+++ Example/HBDLembasTest/HBDLembasTestTests/LembasTestRequest.m
@@ -0,0 +1,13 @@
+//
+//  LembasTestRequest.m
+//  HBDLembasTest
+//
+//  Created by Anil Can Baykal on 05/12/14.
+//  Copyright (c) 2014 Anil Can Baykal. All rights reserved.
+//
+
+#import "LembasTestRequest.h"
+
+@implementation LembasTestRequest
+
+@end
